2b16e761045677d68d40e6d76f9f6895da5f8054
TaurusXin <taurusxin@outlook.com>
2022-02-19 15:40:59 +0000

    add time to welcome converter

---
+++ Coffer/Converters/TimeToWelcomeConverter.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Coffer.Converters
+{
+    public class TimeToWelcomeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var time = DateTime.Now.Hour;
+            if (time > 7 && time < 11)
+            {
+                return "Good morning!";
+            } else if (time < 13)
+            {
+                return "Lunch time";
+            } else if (time < 18)
+            {
+                return "Good afternoon!";
+            } else if (time < 23)
+            {
+                return "Good evening!";
+            }
+            else
+            {
+                return "Go to the bed!";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}

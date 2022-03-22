8072414cea37b83471725cb0c0a14cc39e79071e
TaurusXin <taurusxin@outlook.com>
2022-03-22 21:09:26 +0000

    feat: android data

--- Coffer.Droid/Properties/AssemblyInfo.cs
+++ Coffer.Droid/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Coffer.Droid")]
+[assembly: AssemblyTitle("Coffer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CompanyName")]
+[assembly: AssemblyCompany("TaurusXin")]
 [assembly: AssemblyProduct("Coffer.Droid")]
-[assembly: AssemblyCopyright("Copyright © CompanyName Year")]
+[assembly: AssemblyCopyright("Copyright © TaurusXin 2022")]
 [assembly: AssemblyTrademark("CompanyTrademark")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]

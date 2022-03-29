c4e0096f91a4ddfe56b6d1dec8ba2f9bcf36412b
TaurusXin <taurusxin@outlook.com>
2022-03-29 22:34:11 +0100

    add models

--- Coffer/Models/Coffee.cs
+++ Coffer/Models/Coffee.cs
@@ -2,6 +2,8 @@ namespace Coffer.Models
 {
     public class Coffee
     {
-        
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public string CoffeeName { get; set; }
     }
 }
---
+++ Coffer/Models/Content.cs
@@ -0,0 +1,10 @@
+namespace Coffer.Models
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public int CoffeeId { get; set; }
+        public string SizeName { get; set; }
+        public int Caffeine { get; set; }
+    }
+}

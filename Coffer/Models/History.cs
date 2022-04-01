448cfb101080706b35961132a2fc45b74a59866d
TaurusXin <taurusxin@outlook.com>
2022-04-01 23:30:10 +0100

    change model History definition

--- Coffer/Models/History.cs
+++ Coffer/Models/History.cs
@@ -8,8 +8,12 @@ namespace Coffer.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public int ContentId { get; set; }
-        public double Count { get; set; }
-        public double TotalCaffeine { get; set; }
         public DateTime Datetime { get; set; }
+        
+        public string CoffeeName { get; set; }
+        public double Count { get; set; }
+        
+        public string Size { get; set; }
+        public double TotalCaffeine { get; set; }
     }
 }
